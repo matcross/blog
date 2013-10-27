1babce6f771219d17b957ec10eb0be7605803223
Mat Cross <mathewc@nag_fake.co_fake.uk_fake>
2013-10-27 14:49:29 +0000

    In customizing-svn-diff/svndiff.sh: use dummy name for exclude_file.

--- customizing-svn-diff/svndiff.sh
+++ customizing-svn-diff/svndiff.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -u
-exclude_file=/fserver/mathewc/scripts/archive_diff_excludes
+exclude_file=/path/to/exclude/file
 svn diff --no-diff-deleted --diff-cmd diff -x "--ignore-all-space --text --unified=0" $* | \
   filterdiff --verbose --exclude-from-file ${exclude_file} | grep -v --file=${exclude_file} | colordiff | less -RS
